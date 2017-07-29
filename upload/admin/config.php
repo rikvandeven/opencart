d8130ab10f900efc00b67ae0d8837a2f2d3e667c
Rik van de Ven <rik@pitahaya.local>
2017-07-29 18:25:24 +0200

    sharing config files

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://samadev/admin/');
+define('HTTP_CATALOG', 'https://samadev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://samadev/admin/');
+define('HTTPS_CATALOG', 'https://samadev/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/rik/Development/git/opencart/upload/admin/');
+define('DIR_SYSTEM', '/Users/rik/Development/git/opencart/upload/system/');
+define('DIR_IMAGE', '/Users/rik/Development/git/opencart/upload/image/');
+define('DIR_LANGUAGE', '/Users/rik/Development/git/opencart/upload/admin/language/');
+define('DIR_TEMPLATE', '/Users/rik/Development/git/opencart/upload/admin/view/template/');
+define('DIR_CONFIG', '/Users/rik/Development/git/opencart/upload/system/config/');
+define('DIR_CACHE', '/Users/rik/Development/git/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/rik/Development/git/opencart/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/rik/Development/git/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/rik/Development/git/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/rik/Development/git/opencart/upload/system/storage/upload/');
+define('DIR_CATALOG', '/Users/rik/Development/git/opencart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart23_prod');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://samadev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://samadev/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/rik/Development/git/opencart/upload/catalog/');
+define('DIR_SYSTEM', '/Users/rik/Development/git/opencart/upload/system/');
+define('DIR_IMAGE', '/Users/rik/Development/git/opencart/upload/image/');
+define('DIR_LANGUAGE', '/Users/rik/Development/git/opencart/upload/catalog/language/');
+define('DIR_TEMPLATE', '/Users/rik/Development/git/opencart/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/rik/Development/git/opencart/upload/system/config/');
+define('DIR_CACHE', '/Users/rik/Development/git/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/rik/Development/git/opencart/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/rik/Development/git/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/rik/Development/git/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/rik/Development/git/opencart/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart23_prod');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
